25624f6d804084b4c68c50dba77bc37cc1d4ec9d
unknown <stateasimffa@gmail.com>
2019-10-17 14:59:07 +0900

    update2

---
+++ app/src/main/java/com/example/android_weather_app/Forecast.kt
@@ -0,0 +1,5 @@
+package com.example.android_weather_app
+
+data class Forecast(val temp: Double, val max_temp: Double, val min_temp: Double, val humid: Double) {
+
+}
--- app/src/main/java/com/example/android_weather_app/MainActivity.kt
+++ app/src/main/java/com/example/android_weather_app/MainActivity.kt
@@ -6,9 +6,10 @@ import android.os.Bundle
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
-import java.net.URL
 import android.os.AsyncTask
-import android.util.Log
+import org.json.JSONArray
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,36 +24,73 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
+        refresh_button.setOnClickListener {
+            update()
+        }
         update()
     }
 
     private fun update(){
         fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
                 if (location != null){
-                        lat = location.latitude
-                        lon = location.longitude
-
-                        var str = URL("http://samples.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=b1608dd44f80ebacd6e4b7ea11bb6cae").readText()
-                        Log.d("retrieves", str);
-//                        val url1 = URL("http://samples.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=b1608dd44f80ebacd6e4b7ea11bb6cae")
-//                        val url2 = URL("")
-//                        val url3 = URL("")
-//                        val url1Connection = url1.openConnection() as HttpURLConnection
-//                        val url2Connection = url2.openConnection() as HttpURLConnection
-//                        val url3Connection = url3.openConnection() as HttpURLConnection
-//                        try {
-//                            city_name.text = ""+url1Connection.responseMessage[0]
-//                        } finally {
-//                            url1Connection.disconnect()
-//                            url2Connection.disconnect()
-//                            url3Connection.disconnect()
-//                        }
+                    lat = location.latitude
+                    lon = location.longitude
+                    val url1 = "http://samples.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=b1608dd44f80ebacd6e4b7ea11bb6cae"
+                    AsyncRequest().execute(url1)
+//                    val url2 = URL("")
+//                    val url3 = URL("")
+//                    val url1Connection = url1.openConnection() as HttpURLConnection
+//                    val url2Connection = url2.openConnection() as HttpURLConnection
+//                    val url3Connection = url3.openConnection() as HttpURLConnection
+//                    try {
+//                        city_name.text = ""+url1Connection.responseMessage[0]
+//                    } finally {
+//                        url1Connection.disconnect()
+//                        url2Connection.disconnect()
+//                        url3Connection.disconnect()
+//                    }
                     //city_name.text = "still here"
                     } else {
-                        //city_name.text = "ERROR! Location services not enabled!"
+                    //city_name.text = "ERROR! Location services not enabled!"
                     }
             }
+    }
+
+    inner class AsyncRequest : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg url: String): String {
+            var text: String = ""
+            var connection = URL(url[0]).openConnection() as HttpURLConnection
+            //var connection = URL("android.com") as HttpURLConnection
+
+            try {
+                //connection.connect()
+                //text = connection.inputStream.use { it.reader().use{reader -> reader.readText()} }
+            } finally {
+                //connection.disconnect()
+            }
+            return text
+        }
+
+        override fun onPostExecute(result: String) {
+            //super.onPostExecute(result)
+            //jsonParse(result)
+        }
+    }
+
+    private fun jsonParse(jsonString: String) {
+//        val jsonArray = JSONArray(jsonString)
+//        val list = ArrayList<Forecast>()
+//        var x = 0
+//        while (x < jsonArray.length()){
+//            val jsonObject = jsonArray.getJSONObject(x)
+//            list.add(Forecast(
+//                    jsonObject.getDouble("temp"),
+//                    jsonObject.getDouble("temp_max"),
+//                    jsonObject.getDouble("temp_min"),
+//                    jsonObject.getDouble("humidity")
+//            ))
+//            x++
+//        }
     }
 
     private fun updateCurrent(){
